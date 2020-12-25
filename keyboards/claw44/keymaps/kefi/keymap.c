1baa2eca511cd98fd6f339bed8043141ef360271
kefi550 <ytiryrstd550@gmail.com>
2020-12-26 05:15:37 +0900

    claw44

--- keyboards/claw44/keymaps/kefi/keymap.c
+++ keyboards/claw44/keymaps/kefi/keymap.c
@@ -11,6 +11,7 @@ enum layer_number {
     _QWERTY = 0,
     _RAISE,
     _LOWER,
+    _SHIFT,
 };
 
 #define KC_ KC_TRNS
@@ -25,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        KC_LCTL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_COLN, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LGUI , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+       KC_LGUI , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RCTL,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                         KC_LALT ,KC_R_SPC, KC_LSFT, KC_ESC ,     KC_RCTL, KC_ENT ,KC_L_TAB, KC_RALT
+                         KC_LALT ,MO(_RAISE),KC_SPC, KC_LSFT,     KC_RSFT, KC_ENT ,MO(_LOWER),KC_RALT
     //                 `---------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 
@@ -35,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
        KC_TAB  , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_ESC,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LCTL , KC_DQUO, KC_PIPE, KC_TILD, KC_BTN1, KC_BTN2,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_BSPC,
+       KC_LCTL , KC_DQUO, KC_PIPE, KC_TILD, KC_BTN1, KC_BTN2,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_SCLN, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LGUI , _______, _______, _______, KC_UNDS, KC_LCBR,     KC_RCBR, KC_PLUS, KC_LT  , KC_GT  , KC_QUES, KC_RALT,
+       KC_LGUI , _______, _______, _______, KC_UNDS, KC_LCBR,     KC_RCBR, KC_PLUS, KC_LT  , KC_GT  , KC_QUES, KC_RCTL,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                         KC_LALT ,KC_R_SPC, KC_LSFT, KC_ESC ,     KC_RCTL, KC_ENT ,KC_L_TAB, KC_RALT
+                         KC_LALT , KC_NO  , KC_SPC , KC_NO  ,     KC_NO  ,KC_ENT ,MO(_SHIFT),KC_RALT
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 
@@ -49,9 +50,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        KC_LCTL , KC_QUOT, KC_BSLS, KC_GRV , KC_BTN1, KC_BTN2,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_SCLN, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LGUI , _______, _______, _______, KC_MINS, KC_LBRC,     KC_RBRC, KC_EQL , KC_COMM, KC_DOT , KC_SLSH, KC_RALT,
+       KC_LGUI , _______, _______, _______, KC_MINS, KC_LBRC,     KC_RBRC, KC_EQL , KC_COMM, KC_DOT , KC_QUES, KC_RCTL,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                         KC_LALT ,KC_R_SPC, KC_LSFT, KC_ESC ,     KC_RCTL, KC_ENT ,KC_L_TAB, KC_RALT
+                         KC_LALT ,MO(_SHIFT),KC_SPC, KC_NO  ,     KC_NO  , KC_ENT , KC_NO  , KC_RALT
+    //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
+    ),
+
+    [_SHIFT] = LAYOUT( \
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       KC_TAB  , S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T),     S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), KC_RST,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LCTL , S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),     S(KC_H), S(KC_J), S(KC_K), S(KC_L),S(KC_COLN),KC_BSPC,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LGUI , S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),     S(KC_N), S(KC_M), S(KC_COMM),S(KC_DOT),S(KC_SLSH),KC_RCTL,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+                         KC_LALT , KC_NO  , KC_SPC , KC_NO  ,     KC_NO  , KC_ENT , KC_NO  , KC_RALT
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 };
